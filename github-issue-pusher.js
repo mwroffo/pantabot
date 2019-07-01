229e4c52892d793b49590f798364c89245156106
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-01 14:58:09 -0400

    implemented try-catch
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- github-issue-pusher.js
+++ github-issue-pusher.js
@@ -7,7 +7,7 @@ const GITHUB_AUTH = require('./myauth.js').GITHUB_AUTH_BASIC;
 * @param {GithubIssue} [github_issue] - a json object specifying the issue to be posted to github.
 * @return {Promise} - the promise for the http request to the github api.
 */
-function postIssue(issue, org_or_user, repo) {
+async function postIssue(issue, org_or_user, repo) {
     // post the issue:
     console.log(`POSTING ISSUE:`);
     console.log(issue);
@@ -17,5 +17,10 @@ function postIssue(issue, org_or_user, repo) {
     // `Issue` wrapper, which extends `Requestable`, which encapsulates 
     // a username/password pair or oauth token for github
     const Issue = gh.getIssues(GITHUB_AUTH.username, repo);
-    Issue.createIssue(issue).then(data => console.log(data)).catch(err => {throw err;});
+    // Issue.createIssue(issue).then(data => console.log(data)).catch(err => {throw err;});
+    try {
+        const json_response = await Issue.createIssue(issue);
+        console.log(json_response);
+        return json_response;
+    } catch (err) {throw err;}
 }
