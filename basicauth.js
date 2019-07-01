d618cab15403110b038cd3634596a62eda8888eb
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-01 10:37:35 -0400

    added instructions to basicauth.js
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- basicauth.js
+++ basicauth.js
@@ -1,3 +1,6 @@
+// copy this file as myauth.js then replace with your login credentials
+// then `echo "myauth.js" >> ./.gitignore`
+
 module.exports.JIRA_AUTH_BASIC = {
     username:'jsmith',
     password:'password'
