51d0d3a3a438a661dc666b51fad5a1a3b3cac71e
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-01 11:17:07 -0400

    added first j2g command
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+'use strict';
+
 const JiraIssuePuller = require('./jira-issue-puller');
 const Jira2GithubConverter = require('./jira-to-github-converter');
+const program = require('commander');
+const pkg = require('./package.json')
 
-function main() {
-    JiraIssuePuller.fetchXML('MVD-3049', Jira2GithubConverter.handleXML);
+function setupCLI() {
+    program
+        .version(pkg.version)
+        .description(pkg.description)
+        .usage('[options] <command> [...]');
+    program
+        .command('j2g <JIRA_ISSUE_CODE> <ZOWE_REPO_PATH>')
+        .description('Given a Jira issue as \'MVD-3048\', \'MVD-3051\', etc., posts that Jira issue to the specified zowe repo on GitHub.')
+        .action(JiraIssuePuller.fetchXML('MVD-3049', Jira2GithubConverter.handleXML));
+    program.parse(process.argv);
+    if (!program.args.filter(arg => typeof arg === 'object').length) {
+        // if bad args,
+        program.help(); // show help
+    }
+    
 }
 
 // if this module is imported somewhere else, do not run main
 if (!module.parent) {
-    main();
+    setupCLI();
 }
