e13673ef7163c3011fe66e4565bc63b5f1649c1c
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-01 14:18:50 -0400

    refactor for CLI
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- jira-issue-puller.js
+++ jira-issue-puller.js
@@ -5,7 +5,8 @@ const JIRA_AUTH = require('./myauth.js').JIRA_AUTH_BASIC;
 * Takes a jira.rocketsoftware.com issue id and returns the issue in xml form
 * @param {string} [issue_id] the issue's id, for example MVD-3017
 */
-function fetchXML(issue_id, handleXML) {
+function fetchXML(handleXML) {
+    const issue_id = process.argv[3];
     const url = `https://jira.rocketsoftware.com/si/jira.issueviews:issue-xml/${issue_id}/${issue_id}.xml`;
     console.log(`fetching xml from ${url}`);
     const options = {
