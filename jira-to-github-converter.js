314f93ace51fa3600257d7a38da99ccbfe336420
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-01 14:52:48 -0400

    asynch/await-ify convertXMLIssue2GithubIssue function
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- jira-to-github-converter.js
+++ jira-to-github-converter.js
@@ -18,27 +18,21 @@ function convert2GithubIssue(jira_issue) {
     return github_issue;
 }
 
-function handleXML(err,response,body_xml) {
-    if (err) throw err;
-    else {
-        // convertXMLIssue2GithubIssue
-        let githubissue = {};
-        // console.log(body_xml);
-        const $ = cheerio.load(body_xml, {xml: {normalizeWhitespace: true}});
-        githubissue.title = $('item title').text();
-        // TODO remove html tags from body
-        githubissue.body = $('item description').text();
-        // jira allows one assignee whereas github allows an array of assignees
-        githubissue.assignees = [$('item assignee').text()];
-        // dense, but each label element must be convert to string separately:
-        githubissue.labels = $('item labels').toArray().map(elem => $(elem).text());
-        // console.log('RESULTING GITHUB ISSUE IS:\n', githubissue);
-        // TODO june27 2019 labels should appear as strings in separate array indices
-        
-        // post github issue
-        console.log(`ISSUE CONVERTED ${githubissue}`);
-        const username_or_org = progress.argv[4];
-        const repo = process.argv[5];
-        GithubIssuePusher.postIssue(githubissue, username_or_org, repo);
-    }
+function convertXMLIssue2GithubIssue(body_xml) {
+    let githubissue = {};
+    // console.log(body_xml);
+    const $ = cheerio.load(body_xml, {xml: {normalizeWhitespace: true}});
+    githubissue.title = $('item title').text();
+    // TODO remove html tags from body
+    githubissue.body = $('item description').text();
+    // jira allows one assignee whereas github allows an array of assignees
+    githubissue.assignees = [$('item assignee').text()];
+    // dense, but each label element must be convert to string separately:
+    githubissue.labels = $('item labels').toArray().map(elem => $(elem).text());
+
+    // TODO june27 2019 labels should appear as strings in separate array indices
+    
+    // post github issue
+    console.log(`issue converted ${githubissue}`);
+    return githubissue;
 }
