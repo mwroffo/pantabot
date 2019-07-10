afe89da17400c67a7857503319a517c7f6452914
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-10 14:55:21 -0400

    updated boilerplate to invite token auth
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- config-boilerplate.js
+++ config-boilerplate.js
@@ -1,11 +1,12 @@
 // copy this file as myauth.js then replace with your login credentials
 // then `echo "myauth.js" >> ./.gitignore`
 
-module.exports.JIRA_AUTH_BASIC = {
+module.exports.JIRA_CONF = {
     username:'jsmith',
     password:'password'
 }
-module.exports.GITHUB_AUTH_BASIC = {
+module.exports.GITHUB_CONF = {
     username:'jsmith',
-    password:'password'
+    password:'password',
+    token: ''
 }
