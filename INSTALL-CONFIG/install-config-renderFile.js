ce01c6ad7dba667e3e90edd0eaeb5d3e304fba23
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-15 13:40:40 -0400

    inited setup applet
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

---
+++ INSTALL-CONFIG/install-config-renderFile.js
@@ -0,0 +1,12 @@
+const ipcRenderer = require('electron').ipcRenderer;
+
+function sendForm(event) {
+    event.preventDefault() // stop the form from submitting
+    const jiraUsername = document.getElementById("jira-username").value;
+    const jiraPassword = document.getElementById("jira-password").value;
+    const githubUsername = document.getElementById("github-username").value;
+    const githubPassword = document.getElementById("github-password").value;
+    issues = issues.split(' ');
+    ipcRenderer.send('register-auth',
+        jiraUsername, jiraPassword, githubUsername, githubPassword);
+}
---
+++ INSTALL-CONFIG/install-config-ui.js
@@ -0,0 +1,72 @@
+const { app, BrowserWindow, ipcMain  } = require('electron');
+const Panta = require('../panta.js');
+const path = require ('path');
+const fs = require('fs');
+const os = require('os');
+const keytar = require('keytar');
+let GITHUB_AUTH = require('../config.js').GITHUB_AUTH;
+let JIRA_AUTH = require('../config.js').JIRA_AUTH;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let window;
+
+function createWindow () {
+  // Create the browser window.
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    show: false
+  })
+
+  window.loadURL(`file://${__dirname}/index.html`);
+  window.once('ready-to-show', function (){
+    window.show();
+  });
+
+  // Open the DevTools.
+  // window.webContents.openDevTools()
+  
+  let contents = window.webContents;
+  // Emitted when the window is closed.
+  window.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    window = null;
+  })
+} 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+function buildUI() {
+
+    app.on('ready', createWindow)
+
+    // Quit when all windows are closed.
+    app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    })
+
+    app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+        if (window === null) {
+            createWindow()
+        }
+    });
+
+    ipcMain.on('register-auth', async function registerPasswords(
+        event, jiraUsername, jiraPassword, githubUsername, githubPassword) {
+          await keytar.setPassword('jira', jiraUsername, jiraPassword);
+          await keytar.setPassword('github', githubUsername, githubPassword);
+    });
+}
+buildUI();
