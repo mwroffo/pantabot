f62343f228d59f15465a563137db3a32908bcec0
Michael W Roffo <mroffo@rocketsoftware.com>
2019-07-15 13:42:56 -0400

    now auth is sent separatedly
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- renderFile.js
+++ renderFile.js
@@ -2,17 +2,9 @@ const ipcRenderer = require('electron').ipcRenderer;
 
 function sendForm(event) {
     event.preventDefault() // stop the form from submitting
-    const jiraUsername = document.getElementById("jira-username").value;
-    const jiraPassword = document.getElementById("jira-password").value;
-    const githubUsername = document.getElementById("github-username").value;
-    const githubPassword = document.getElementById("github-password").value;
-    const rememberJiraPasswordIsChecked = document.getElementById("jira-password-remember").value;
-    const rememberGithubPasswordIsChecked = document.getElementById("github-password-remember").value;
     const owner = document.getElementById("owner").value;
     const repo = document.getElementById("repo").value;
     let issues = document.getElementById("jira-issue-codes").value;
     issues = issues.split(' ');
-    ipcRenderer.send('form-submission',
-        jiraUsername, jiraPassword, rememberJiraPasswordIsChecked, githubUsername, githubPassword, rememberGithubPasswordIsChecked,
-        owner, repo, issues);
+    ipcRenderer.send('form-submission', owner, repo, issues);
 }
