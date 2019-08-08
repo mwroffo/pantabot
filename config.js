8e58650117916a0ad58ad908135e5bb97591010a
Michael W Roffo <mroffo@rocketsoftware.com>
2019-08-08 14:05:24 -0400

    no longer includes sensitive info
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- config.js
+++ config.js
@@ -4,16 +4,17 @@ const keytar = require('keytar');
 
 async function getAuth() {
     let JIRA_CONF = {};
-    JIRA_CONF.username = 'jsmith';
+    JIRA_CONF.BASE_URI = "jira.rocketsoftware.com";
+    JIRA_CONF.username = "mroffo";
     const jiraPassword = await keytar.getPassword('jira', JIRA_CONF.username);
     JIRA_CONF.password = jiraPassword;
     let GITHUB_CONF = {};
-    GITHUB_CONF.username = 'jsmith';
+    GITHUB_CONF.username = "mwroffo";
     const githubPassword = await keytar.getPassword('github', GITHUB_CONF.username);
     GITHUB_CONF.password = githubPassword;
     let toReturn = [];
     toReturn.push(JIRA_CONF, GITHUB_CONF);
     return toReturn;
 }
-module.exports.OWNER_REPOS = "octokit/rest.js";
+module.exports.OWNER_REPOS = "mwroffo/testrepo mwroffo/testrepo2 mwroffo/testrepo3";
 module.exports.getAuth = getAuth;
