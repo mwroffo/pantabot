709a253c2963daf518c1492a2c1956ad8b481117
Michael W Roffo <mroffo@rocketsoftware.com>
2019-08-09 16:09:13 -0400

    stripped config file of my test settings
    Signed-off-by: Michael W Roffo <mroffo@rocketsoftware.com>

--- config.js
+++ config.js
@@ -1,16 +1,14 @@
-// Copy this file as config.js then replace with your login credentials
-// Make sure that config.js is in .gitignore!!!
 const keytar = require('keytar');
 
 async function getAuth() {
     try {
         let JIRA_CONF = {};
-        JIRA_CONF.BASE_URI = "jira.rocketsoftware.com";
+        JIRA_CONF.BASE_URI = "jira.organization.com";
         JIRA_CONF.username = "jsmith";
         const jiraPassword = await keytar.getPassword('jira', JIRA_CONF.username);
         JIRA_CONF.password = jiraPassword;
         let GITHUB_CONF = {};
-        GITHUB_CONF.username = "nolanRogers";
+        GITHUB_CONF.username = "jwsmith";
         const githubPassword = await keytar.getPassword('github', GITHUB_CONF.username);
         GITHUB_CONF.password = githubPassword;
         let toReturn = [];
@@ -18,5 +16,5 @@ async function getAuth() {
         return toReturn;
     } catch (err) {throw err}
 }
-module.exports.OWNER_REPOS = "zowe/zlux octokit/rest.js";
+module.exports.OWNER_REPOS = " octokit/rest.js";
 module.exports.getAuth = getAuth;
